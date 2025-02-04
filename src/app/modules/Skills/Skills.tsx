25c094d6e694081c12bdc2f79de06246f172142e
Draggio <lucaspedrodeveloper@gmail.com>
2025-02-04 12:23:20 -0300

    Isso adiciona uma ordenação na listagem de experiências e link nos botões de contato, também foi feito um pequeno ajuste na fonte da listagem de tecnologias

--- src/app/modules/Skills/Skills.tsx
+++ src/app/modules/Skills/Skills.tsx
@@ -3,7 +3,7 @@ interface SkillsProps {
 }
 
 const Skills = ({ data }: SkillsProps) => {
-  return <h4 className="text-w2 text-lg pl-11 mb-4"><span className="text-w1 monoFonting">{`</`}</span> {data.name}</h4>;
+  return <h4 className="text-w2 text-lg pl-11 mb-4 monoFonting"><span className="text-w1 monoFonting">{`</`}</span> {data.name}</h4>;
 }
 
 export default Skills;
